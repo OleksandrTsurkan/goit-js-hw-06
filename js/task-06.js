5686f2dc307939982a1fb147c70f6c953152dca2
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-06-30 16:16:13 +0200

    Update task-06.js

---
+++ js/task-06.js
@@ -0,0 +1,14 @@
+const inputEl = document.querySelector('#validation-input');
+
+const check = (event) => {
+  const input = inputEl.value;
+  if (input.length === Number(inputEl.dataset.length)) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  } else {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  }
+};
+
+inputEl.addEventListener("blur", check);

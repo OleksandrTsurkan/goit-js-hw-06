913dd6da50833f85625cb64d7b297e0a8f6c7bb9
OleksandrTsurkan <126093146+OleksandrTsurkan@users.noreply.github.com>
2023-06-30 07:47:24 +0200

    ategories

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+
+const categoriesList = document.querySelector("ul#categories");
+const totalElements = categoriesList.children.length;
+console.log(`Number of categories: ${totalElements}`);
+
+categoriesList.forEach((category) => {
+    const textHeader = document.querySelector("nh2")textContent;
+    return(67qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq111111111111qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq1a1qa1qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqtr   querySelectorqtr    qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq 

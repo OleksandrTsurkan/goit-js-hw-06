0fd86680f65b905f1483600a950540927abe2ccb
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-06-30 14:19:29 +0200

    Update task-05.js

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+const inputValue = document.querySelector("input");
+const spanValue = document.querySelector("span");
+
+inputValue.addEventListener("input", onInputChange);
+
+function onInputChange (event) {
+  spanValue.textContent = event.currentTarget.value
+};

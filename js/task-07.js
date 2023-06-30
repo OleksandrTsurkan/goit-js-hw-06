3ffc702a5a89d5c345d4fe4e3ca8fd6c9f3c66ea
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-06-30 16:26:00 +0200

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const inputValue = document.querySelector("input#font-size-control");
+
+const textValue = document.querySelector("#text");
+
+const fontSizeChange = () => {
+  const size = inputValue.value;
+  textValue.style.fontSize = `${size}px`; 
+}
+
+inputValue.addEventListener('input',fontSizeChange);

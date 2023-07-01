5af2b205064adeedc24515bdb4030a759de7ef04
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-01 13:41:14 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonAction = document.querySelector(".change-color");
+const spanAction = document.querySelector("span");
+const bodyAction = document.querySelector("body");
+
+buttonAction.addEventListener("click", onChangeColor);
+
+function onChangeColor () {
+  const randomColor = getRandomHexColor();
+  bodyAction.style.backgroundColor = randomColor;
+  spanAction.textContent = `${randomColor}`;
+}

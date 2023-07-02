f40e1e959456804a756f579ece4354ef4aea8e04
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-02 12:08:40 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
 const addButton = document.querySelector('button[data-action="increment"]');
 const removeButton = document.querySelector('button[data-action="decrement"]');
-const calcValue = document.querySelector('#value');
-let counterValue= Number(calcValue.textContent);
+const calcValue = document.querySelector('span');
+let counterValue = Number(calcValue.textContent);
 
 const firstCallback = () => {
   counterValue += 1;

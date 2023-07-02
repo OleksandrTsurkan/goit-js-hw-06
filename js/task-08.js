2e77be3776dd560dc85184d50cb52684b0e1afcc
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2023-07-02 11:58:57 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,14 +1,18 @@
+const button = document.querySelector("button");
+const form = document.querySelector("form");
 
-const form = document.querySelector(".login-form");
 form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
+ 
   event.preventDefault();
+  
   const {
-    elements: { login, password }
+    elements: { email, password }
   } = event.currentTarget;
-  if (login.value === "" || password.value === "") {
+  
+  if (email.value === "" || password.value === "") {
     return console.log("Please fill in all the fields!");
-  } console.log(`Login: ${login.value}, Password: ${password.value}`);
+  } console.log(`Login: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
 };
